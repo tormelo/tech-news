77883871d7ed653656f0bf8ebc50579b86bf4a3e
tormelo <23294699+tormelo@users.noreply.github.com>
2023-04-13 20:50:58 -0300

    feat: add top_5_categories method to ratings.py module

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,15 @@
+from tech_news.database import db
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    search_result = list(
+        db.news.aggregate(
+            [
+                {"$group": {"_id": "$category", "count": {"$sum": 1}}},
+                {"$sort": {"count": -1, "_id": 1}},
+            ]
+        )
+    )
+
+    return [result["_id"] for result in search_result[:5]]
