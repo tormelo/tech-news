052b683d36402cf409fab4e6321cf07f30b4a596
tormelo <23294699+tormelo@users.noreply.github.com>
2023-04-13 19:27:07 -0300

    feat: add search_by_date method to search_engine.py module

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import db
+from datetime import datetime
 
 
 # Requisito 7
@@ -6,7 +7,7 @@ def search_by_title(title):
     search_result = list(
         db.news.find(
             {"title": {"$regex": title, "$options": "i"}},
-            {"title": 1, "url": 1, "_id": 0},
+            # {"title": 1, "url": 1, "_id": 0},
         )
     )
 
@@ -15,7 +16,20 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        q_datetime = datetime.strptime(date, "%Y-%m-%d")
+        q_date = q_datetime.strftime("%d/%m/%Y")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    search_result = list(
+        db.news.find(
+            {"timestamp": q_date},
+            # {"title": 1, "url": 1, "_id": 0},
+        )
+    )
+
+    return [(news["title"], news["url"]) for news in search_result]
 
 
 # Requisito 9
