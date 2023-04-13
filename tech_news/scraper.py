cd76e225e4b1e1673064a265f48dca8c71c966bb
tormelo <23294699+tormelo@users.noreply.github.com>
2023-04-13 15:09:40 -0300

    feat: add scrape_updates method to scraper.py module

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from parsel import Selector
 
 
 # Requisito 1
@@ -19,7 +20,8 @@ def fetch(url):
 
 # Requisito 2
 def scrape_updates(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    return selector.css("h2.entry-title > a::attr(href)").getall()
 
 
 # Requisito 3
